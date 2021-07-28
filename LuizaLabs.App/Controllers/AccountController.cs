ef91494c550005dc868135a3465186f9764bf2a7
Claudio Volnei <claudiovolneisilva@gmail.com>
2021-07-28 20:47:05 -0300

    Atualização de projeto com authenticação .net core mvc

---
+++ LuizaLabs.App/Controllers/AccountController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LuizaLabs.App.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using LuizaLabs.App.ValuesObjects;
+using System.Security.Claims;
+
+namespace LuizaLabs.App.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                // Use Input.Email and Input.Password to authenticate the user
+                // with your custom authentication logic.
+                //
+                // For demonstration purposes, the sample validates the user
+                // on the email address maria.rodriguez@contoso.com with 
+                // any password that passes model validation.
+
+                var user = await AuthenticateUser(usuario);
+
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View("Login");
+                }
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim("Nome", user.Nome),
+                    new Claim(ClaimTypes.Role, "Administrator"),
+                };
+
+                var claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties
+                {
+                    //AllowRefresh = <bool>,
+                    // Refreshing the authentication session should be allowed.
+
+                    //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                    // The time at which the authentication ticket expires. A 
+                    // value set here overrides the ExpireTimeSpan option of 
+                    // CookieAuthenticationOptions set with AddCookie.
+
+                    //IsPersistent = true,
+                    // Whether the authentication session is persisted across 
+                    // multiple requests. When used with cookies, controls
+                    // whether the cookie's lifetime is absolute (matching the
+                    // lifetime of the authentication ticket) or session-based.
+
+                    //IssuedUtc = <DateTimeOffset>,
+                    // The time at which the authentication ticket was issued.
+
+                    //RedirectUri = <string>
+                    // The full path or absolute URI to be used as an http 
+                    // redirect response value.
+                };
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+
+                _logger.LogInformation("User {Email} logged in at {Time}.",
+                    user.Email, DateTime.UtcNow);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return RedirectToAction("Login", "Account");
+
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Login");
+        }
+
+        private async Task<Usuario> AuthenticateUser(Usuario usuario)
+        {
+            // For demonstration purposes, authenticate a user
+            // with a static email address. Ignore the password.
+            // Assume that checking the database takes 500ms
+
+            await Task.Delay(500);
+
+            if (usuario.Email == "claudiovolneisilva@gmail.com")
+            {
+                return new Usuario()
+                {
+                    Email = "claudiovolneisilva@gmail.com",
+                    Nome = "Claudio Volnei"
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ LuizaLabs.App/ValuesObjects/Usuario.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LuizaLabs.App.ValuesObjects
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Token { get; set; }
+    }
+}
